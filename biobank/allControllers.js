cf1c2b4a026bc3cefe92d35975c871392f597a74
Nico Cernek <ncernek@gmail.com>
2015-11-25 21:00:05 -0600

    moved js to controller

--- biobank/allControllers.js
+++ biobank/allControllers.js
@@ -1,30 +1,113 @@
 'use strict';
 
-var allControllers = angular.module('allControllers', []);
+var app = angular.module('chartApp', []);
 
-allControllers.factory('initvalues', [function () {
-    return {sets:
-        [
-            {id:"A", size:"50"},
-            {id:"B", size:"30"},
-            {id:"C", size:"20"},
-            {id:"A,B", size:"10"},
-            {id:"B,C", size:"10"},
-            {id:"A,C", size:"10"},
-            {id:"A,B,C", size:"5"}
-            //{id:"A", size:50},
-            //{id:"B", size:30},
-            //{id:"C", size:20},
-            //{id:"A,B", size:10},
-            //{id:"B,C", size:10},
-            //{id:"A,C", size:10},
-            //{id:"A,B,C", size:5}
-        ]
+app.controller('SalesController', ['$scope', function ($scope) {
+
+    var a = $scope.a = 50;
+    var b = $scope.b = 30;
+    var c = $scope.c = 20;
+    var ab = $scope.ab = 10;
+    var bc = $scope.bc = 10;
+    var ac = $scope.ac = 10;
+    var abc = $scope.abc = 5;
+
+    $scope.salesData = [
+        {sets:"A", size: $scope.a},
+        {sets:"B", size: $scope.b},
+        {sets:"C", size: $scope.c},
+        {sets:"A,B", size: $scope.ab},
+        {sets:"B,C", size: $scope.bc},
+        {sets:"A,C", size: $scope.ac},
+        {sets:"A,B,C", size: $scope.abc}
+    ];
+
+
+
+
+    var getSetIntersections = function() {
+        return $scope.salesData.map(
+            function (element) {
+                return { sets: element.sets.split(","),
+                    size: parseInt(element.size)};
+            });
     };
+
+    var chart = venn.VennDiagram()
+        .width(500)
+        .height(500);
+
+    var div = d3.select("#venn");
+
+    div.datum(getSetIntersections()).call(chart);
+
+    d3.selectAll("input").on("change", function () {
+        console.log("changed:", getSetIntersections());
+        div.datum(getSetIntersections()).call(chart);
+    });
+
+    var tooltip = d3.select("body").append("div")
+        .attr("class", "venntooltip");
+
+    div.selectAll("path")
+        .style("stroke-opacity", 0)
+        .style("stroke", "#fff")
+        .style("stroke-width", 0);
+
+    div.selectAll("g")
+        .on("mouseover", function (d) {
+            // sort all the areas relative to the current item
+            venn.sortAreas(div, d);
+
+            // Display a tooltip with the current size
+            tooltip.transition().duration(400).style("opacity", .9);
+            tooltip.text(d.size + " users");
+
+            // highlight the current path
+            var selection = d3.select(this).transition("tooltip").duration(400);
+            selection.select("path")
+                .style("stroke-width", 3)
+                .style("fill-opacity", d.sets.length == 1 ? .4 : .1)
+                .style("stroke-opacity", 1);
+        })
+
+        .on("mousemove", function () {
+            tooltip.style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
+        })
+
+        .on("mouseout", function (d) {
+            tooltip.transition().duration(400).style("opacity", 0);
+            var selection = d3.select(this).transition("tooltip").duration(400);
+            selection.select("path")
+                .style("stroke-width", 0)
+                .style("fill-opacity", d.sets.length == 1 ? .25 : .0)
+                .style("stroke-opacity", 0);
+        });
+
 }]);
 
-allControllers.controller('MainCtrl', ['$scope', 'initvalues', function ($scope, initvalues) {
-    $scope.sets = initvalues.sets;
 
-}]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- biobank/app.js
+++ biobank/app.js
@@ -1,5 +1,5 @@
 'use strict';
 
 var vennApp = angular.module('vennApp', [
-    'allControllers'
+    'chartApp'
 ]);
